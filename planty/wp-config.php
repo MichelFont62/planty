d731ba0af4d7c0d7eebc86d2125ebcc2b43ee174
Michel <michelfontaine2013@laposte.net>
2024-08-09 10:06:49 +0200

    home fait version pc

--- planty/wp-config.php
+++ planty/wp-config.php
@@ -84,8 +84,8 @@ $table_prefix = 'wp_';
  *
  * @link https://fr.wordpress.org/support/article/debugging-in-wordpress/
  */
-define('WP_DEBUG', false);
-
+define('WP_DEBUG', true);
+define('WP_MEMORY_LIMIT','512M');
 /* C’est tout, ne touchez pas à ce qui suit ! Bonne publication. */
 
 /** Chemin absolu vers le dossier de WordPress. */
---
+++ planty/wp-content/themes/blankslate_enfant/footer.php
@@ -0,0 +1,10 @@
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer id="footer" role="contentinfo">
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>">Mentions légales</a>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
